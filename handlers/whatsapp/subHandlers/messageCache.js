559e1d37d59b18081e0d666e0e39c026a349d322
ILoveC4t <109042057+ILoveC4t@users.noreply.github.com>
2023-03-08 00:09:31 +0100

    Bbbbut it works on my pc

--- handlers/whatsapp/subHandlers/messageCache.js
+++ handlers/whatsapp/subHandlers/messageCache.js
@@ -2,6 +2,7 @@
 const sqllite = require('better-sqlite3');
 const path = require('path');
 const nodeSchedule = require('node-schedule');
+const fse = require('fs-extra');
 const { MessageTypes } = require('whatsapp-web.js');
 const gpt3encoder = require('gpt-3-encoder');
 
@@ -10,6 +11,7 @@ const gpt3encoder = require('gpt-3-encoder');
 const schedule = nodeSchedule.scheduleJob('0 * * * *', cleanup);
 
 const dbPath = path.join(projectRoot, 'cache', 'cache.db');
+await fse.ensureDir(path.join(projectRoot, 'cache'));
 const db = sqllite(dbPath);
 
 db.prepare('CREATE TABLE IF NOT EXISTS chats (chatID TEXT PRIMARY KEY)').run();
